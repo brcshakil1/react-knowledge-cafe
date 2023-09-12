d1d8b5ecbdf6796ff9f845fd6764ac1242eda865
brcshakil1 <brcshakil72@gmail.com>
2023-09-12 12:39:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 
+      className='text-3xl text-red-400'
+      >
+        Knowledge Cafe
+      </h1>
+      <h3 className=''></h3>
+    </>
+  )
+}
+
+export default App

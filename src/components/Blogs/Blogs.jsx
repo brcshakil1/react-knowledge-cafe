4971ed8b634dc25162f8ec5971ee2594d6ec0c6d
brcshakil1 <brcshakil72@gmail.com>
2023-09-12 16:33:33 +0600

    showed each blog

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
@@ -8,12 +9,14 @@ const Blogs = () => {
     useEffect(()=> {
         fetch('blogs.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setBlogs(data))
     },[])
 
     return (
         <header className="md:w-2/3">
-            <h1>Blogs</h1>
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+
+            {blogs.map(blog => <Blog key={blog.iD} blog={blog}></Blog>)}
         </header>
     );
 };
